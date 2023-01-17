dfce6d642a998115abc48ecaf104fb30ec7d0bca
Ryan Spears <sperj001@hotmail.com>
2023-01-18 09:58:24 +1030

    Added styling to the Account page

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,96 +1,101 @@
 <script setup>
-  import { supabase } from '../supabase'
-  import { onMounted, ref, toRefs } from 'vue'
+import { supabase } from '../supabase'
+import { onMounted, ref, toRefs } from 'vue'
 
-  const props = defineProps(['session'])
-  const { session } = toRefs(props)
+const props = defineProps(['session'])
+const { session } = toRefs(props)
 
-  const loading = ref(true)
-  const username = ref('')
+const loading = ref(true)
+const username = ref('')
 
-  onMounted(() => {
+onMounted(() => {
     getProfile()
-  })
+})
 
-  async function getProfile() {
+async function getProfile() {
     try {
-      loading.value = true
-      const { user } = session.value
+        loading.value = true
+        const { user } = session.value
 
-      let { data, error, status } = await supabase
-        .from('profiles')
-        .select(`username`)
-        .eq('id', user.id)
-        .single()
+        let { data, error, status } = await supabase
+            .from('profiles')
+            .select(`username`)
+            .eq('id', user.id)
+            .single()
 
-      if (error && status !== 406) throw error
+        if (error && status !== 406) throw error
 
-      if (data) {
-        username.value = data.username
-      }
+        if (data) {
+            username.value = data.username
+        }
     } catch (error) {
-      alert(error.message)
+        alert(error.message)
     } finally {
-      loading.value = false
+        loading.value = false
     }
-  }
+}
 
-  async function updateProfile() {
+async function updateProfile() {
     try {
-      loading.value = true
-      const { user } = session.value
+        loading.value = true
+        const { user } = session.value
 
-      const updates = {
-        id: user.id,
-        username: username.value,
-        updated_at: new Date(),
-      }
+        const updates = {
+            id: user.id,
+            username: username.value,
+            updated_at: new Date(),
+        }
 
-      let { error } = await supabase.from('profiles').upsert(updates)
+        let { error } = await supabase.from('profiles').upsert(updates)
 
-      if (error) throw error
+        if (error) throw error
     } catch (error) {
-      alert(error.message)
+        alert(error.message)
     } finally {
-      loading.value = false
+        loading.value = false
     }
-  }
+}
 
-  async function signOut() {
+async function signOut() {
     try {
-      loading.value = true
-      let { error } = await supabase.auth.signOut()
-      if (error) throw error
+        loading.value = true
+        let { error } = await supabase.auth.signOut()
+        if (error) throw error
     } catch (error) {
-      alert(error.message)
+        alert(error.message)
     } finally {
-      loading.value = false
+        loading.value = false
     }
-  }
+}
 </script>
 
 <template>
-  <form class="form-widget" @submit.prevent="updateProfile">
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" :value="session.user.email" disabled />
-    </div>
-    <div>
-      <label for="username">Name</label>
-      <input id="username" type="text" v-model="username" />
-    </div>
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+            <form class="mt-8 space-y-6" @submit.prevent="updateProfile">
+                <div class="-space-y-px rounded-md shadow-sm w-full">
+                    <div>
+                        <label for="email-address">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email"
+                               :value="session.user.email" disabled>
+                    </div>
+                </div>
+                <div class="-space-y-px rounded-md shadow-sm w-full">
+                    <div>
+                        <label for="username">Name</label>
+                        <input id="username" name="username" type="text" v-model="username">
+                    </div>
+                </div>
 
-    <div>
-      <input
-        type="submit"
-        class="button primary block"
-        :value="loading ? 'Loading ...' : 'Update'"
-        :disabled="loading"
-      />
-    </div>
+                <div>
+                    <input type="submit" class="button primary block" :value="loading ? 'Loading ...' : 'Update'"
+                           :disabled="loading" />
+                </div>
 
-    <div>
-      <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
+                <div>
+                    <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
+                </div>
+            </form>
+        </div>
     </div>
-  </form>
 </template>
--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,44 +1,53 @@
 <script setup>
-  import { ref } from 'vue'
-  import { supabase } from '../supabase'
+import { ref } from 'vue'
+import { supabase } from '../supabase'
 
-  const loading = ref(false)
-  const email = ref('')
+const loading = ref(false)
+const email = ref('')
 
-  const handleLogin = async () => {
-    try {
-      loading.value = true
-      const { error } = await supabase.auth.signInWithOtp({
-        email: email.value,
-      })
-      if (error) throw error
-      alert('Check your email for the login link!')
-    } catch (error) {
-      if (error instanceof Error) {
-        alert(error.message)
-      }
-    } finally {
-      loading.value = false
+const handleLogin = async () => {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithOtp({
+      email: email.value,
+    })
+    if (error) throw error
+    alert('Check your email for the login link!')
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message)
     }
+  } finally {
+    loading.value = false
   }
+}
 </script>
 
 <template>
-  <form class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
-      <h1 class="header">Supabase + Vue 3</h1>
-      <p class="description">Sign in via magic link with your email below</p>
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
       <div>
-        <input class="inputField" type="email" placeholder="Your email" v-model="email" />
+        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+             alt="Your Company">
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in via magic link with your
+          email below</h2>
       </div>
-      <div>
-        <input
-          type="submit"
-          class="button block"
-          :value="loading ? 'Loading' : 'Send magic link'"
-          :disabled="loading"
-        />
-      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST" @submit.prevent="handleLogin">
+        <input type="hidden" name="remember" value="true">
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" v-model="email" required
+                   placeholder="Email address">
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="button primary block" :disabled="loading">
+            Send magic link
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
+  </div>
 </template>
