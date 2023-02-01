ef65d2e54f22d30dc49df65177aa4b46d63f6508
Ryan Spears <sperj001@hotmail.com>
2023-02-01 18:30:30 +1030

    Removed 'New' buttons from Books and Monsters

--- src/components/Monsters.vue
+++ src/components/Monsters.vue
@@ -20,13 +20,6 @@ onMounted(() => {
   <div v-if="monsters" class="flex flex-col min-h-full items-center justify-center">
     <h1>
       Monsters
-      <span>
-        <a :href="'/monsters/new'"
-           class="bg-blue-100 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-white"
-           :disabled="loading">
-          New
-        </a>
-      </span>
     </h1>
     <table class="md:min-w-[80%] text-center content-center">
       <thead class="border-b bg-gray-800">
